a94b6f9ec8c2b80dfc1b8c9f064d9a36de526810
Kausar Hasan <kausarhsn92@gmail.com>
2023-10-02 15:24:24 +0600

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components"
 
+const H1=styled.h1`
+  font-size: 20px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+`
+const Button = styled.button`
+  font-size: 1.4rem;
+  font-weight:400;
+  border:none;
+  background-color: red;
+  padding: 0.8rem 1.2rem;
+`
+
+const StyledApp = styled.div`
+  background-color: green;
+`
 const App = () => {
   return (
-    <div>App</div>
+    <StyledApp>
+      <H1>This is Noting</H1>
+      <Button>Click</Button>
+    </StyledApp>
   )
 }
 
